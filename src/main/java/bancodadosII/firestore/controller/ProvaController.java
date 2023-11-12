668a5a3cdbd4fff7b98d65df7ca4b6abcb863f23
Rharhuky <rharhuky@gmail.com>
2023-11-12 20:28:53 -0300

    add: atualizar, obterTodas e remover prova

--- src/main/java/bancodadosII/firestore/controller/ProvaController.java
+++ src/main/java/bancodadosII/firestore/controller/ProvaController.java
@@ -2,13 +2,9 @@ package bancodadosII.firestore.controller;
 
 import bancodadosII.firestore.model.Prova;
 import bancodadosII.firestore.service.ProvaService;
-import io.opencensus.metrics.export.TimeSeries;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
-import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -16,15 +12,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/prova")
 public class ProvaController {
+    /*
+    TODO atualizar pessoa
+    TODO deletar pessoa
+    TODO getAll pessoa - FIXME with Query
+     */
 
     private final ProvaService provaService;
 
     @PostMapping
     public String saveProva(@RequestBody Prova prova) {
-//        prova.setPeriodo(
-//                List.of(Timestamp.valueOf(
-//                        LocalDate.of(2023, 12, 15).atStartOfDay()),
-//                        Timestamp.valueOf(LocalDate.of(2023, 12, 16).atStartOfDay())));
         return provaService.saveProva(prova);
     }
 
@@ -32,5 +29,29 @@ public class ProvaController {
     public Prova buscarProvaPeloId(@PathVariable String idProva) {
         return provaService.buscarProvaPeloId(idProva);
     }
+
+
+    @GetMapping
+    public List<Prova> buscarTodasProvas(){
+
+        return this.provaService.buscarTodasProvas();
+
+    }
+
+    @PutMapping("/{idProva}")
+    public Prova atualizarProvaPeloId(
+            @PathVariable String idProva,
+            @RequestBody Prova prova) {
+
+        return this.provaService.atualizarProvaPeloId(idProva, prova);
+    }
+
+    @DeleteMapping("del/{idProva}")
+    public String deletarProvaPeloId(@PathVariable String idProva){
+
+        return this.provaService.deletarProvaPeloId(idProva);
+
+    }
+
 
 }
--- src/main/java/bancodadosII/firestore/service/ProvaService.java
+++ src/main/java/bancodadosII/firestore/service/ProvaService.java
@@ -2,27 +2,27 @@ package bancodadosII.firestore.service;
 
 import bancodadosII.firestore.model.Prova;
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Log4j2
 @Service
 public class ProvaService {
-
+    /**
+     * TODO updateProva
+     * TODO deleteProva
+     */
     private static final String COLLECTION_NAME = "prova";
 
     @SneakyThrows
     public String saveProva(Prova prova)  {
 
         Firestore fireStoreDB = FirestoreClient.getFirestore();
-
-//      ApiFuture<WriteResult> colecaoApiFuture =  fireStoreDB.collection(COLLECTION_NAME).document(pessoa.getNome()).set(pessoa);
-
         DocumentReference docReferencia = fireStoreDB.collection(COLLECTION_NAME).add(prova).get();
 
         return docReferencia.getId();
@@ -36,12 +36,45 @@ public class ProvaService {
         ApiFuture<DocumentSnapshot> documentSnapshotApiFuture = documentReference.get();
         DocumentSnapshot documentSnapshot = documentSnapshotApiFuture.get();
 
-        try {
-            Integer n = Integer.parseInt("-h");
-        } catch (Exception exception) {
-            log.error("Testando Erro");
-        }
-
         return documentSnapshot.toObject(Prova.class);
+    }
+
+    @SneakyThrows
+    public String deletarProvaPeloId(String idProva){
+
+        Firestore firestoreDB = FirestoreClient.getFirestore();
+        firestoreDB.collection(COLLECTION_NAME).document(idProva).delete();
+        return "Prova com ID : " + idProva + " foi deletada com sucesso";
+
+    }
+
+    @SneakyThrows
+    public Prova atualizarProvaPeloId(String idProva, Prova prova) {
+
+        Firestore fireStoreDB = FirestoreClient.getFirestore();
+        CollectionReference colecaoProvas = fireStoreDB.collection(COLLECTION_NAME);
+        colecaoProvas.document(idProva).set(prova);
+
+        return prova;
+
+    }
+
+    @SneakyThrows
+    public List<Prova> buscarTodasProvas() {
+
+        Firestore fireStoreDB = FirestoreClient.getFirestore();
+        CollectionReference colecaoProvas = fireStoreDB.collection(COLLECTION_NAME);
+
+        ApiFuture<QuerySnapshot> query = colecaoProvas.get();
+        QuerySnapshot querySnapshot = query.get();
+
+
+        List<QueryDocumentSnapshot> provas = querySnapshot.getDocuments();
+
+        return provas.stream()
+                .map((prova) -> prova.toObject(Prova.class))
+                .toList();
+
+
     }
 }
